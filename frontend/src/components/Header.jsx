a2a49808934b666bd6bcddb702b2b5db60719d1a
Renz Cole <164628164+CodeBuddySLTNS@users.noreply.github.com>
2025-02-08 08:10:06 +0800

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
 
   return (
     <header>
-      <div className="menuIcon" onClick={() => toggleMenu(false)}>
+      <div className="menuIcon" onClick={() => toggleMenu(true)}>
         <TiThMenu />
       </div>
       <div className="logo">
