5d8226eeee09872309f17885c01cf7d1f5f5be80
Renz Cole <164628164+CodeBuddySLTNS@users.noreply.github.com>
2025-02-08 08:04:02 +0800

    Update store.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,9 +2,7 @@ import { create } from "zustand";
 
 export const useMainStore = create(set => ({
   toggleMenuStatus: false,
-  toggleMenu: () => {
-    set(state => ({
-      toggleMenuStatus: !state.toggleMenuStatus
-    }));
+  toggleMenu: (status) => {
+    set({ toggleMenuStatus: status });
   }
 }));
